4094eb77a62538272b39e18653926d93f6e74d18
Amit Agarwal <amitaga@microsoft.com>
2017-05-08 13:49:57 -0700

    CNTK v2 library: Remove the unused allowBroadcast parameter in elementwise output shape inference functions.

--- bindings/python/cntk/ops/functions.py
+++ bindings/python/cntk/ops/functions.py
@@ -1182,15 +1182,15 @@ class Function(cntk_py.Function):
         raise ValueError('Cannot load a model that is neither a file nor a byte buffer.')
 
 @typemap
-def register_native_user_function(op_name, module_name, factory_method_name):
+def register_native_user_function(op_id, module_name, factory_method_name):
     '''
     Registers a native user-defined Function that can be subsequently instantiated
     using the 'native_user_function' method.
 
     Args:
-        op_name (str): Name of the native user-defined Function to register.
-         This name must be unique and an error will be reported if it matches
-         the 'op_name' specified for a previously registered native user-defined Function.
+        op_id (str): Unique id of the native user-defined Function to register.
+         This id must be unique and an error will be reported if it matches
+         the 'op_id' specified for any other registered native user-defined Function.
         module_name (str): Name of the module containing the factory method for creating 
          instances of the native user-defined Function being registered. This is typically
          the name of a DLL/so which exports a factory method for creating instances of the
@@ -1199,17 +1199,17 @@ def register_native_user_function(op_name, module_name, factory_method_name):
          user-defined Function being registered. This method must be an exported method of the
          specified module.
     '''
-    return cntk_py.Function_register_native_user_function(op_name, module_name, factory_method_name)
+    return cntk_py.Function_register_native_user_function(op_id, module_name, factory_method_name)
 
 @typemap
-def native_user_function(op_name, operands, attributes=None, user_function_instance_name=''):
+def native_user_function(op_id, operands, attributes=None, user_function_instance_name=''):
     '''
     Creates an instance of a user-defined Function previously registered using the
     'register_native_user_function' method.
 
     Args:
-        op_name (str): Name of the native user-defined Function to instantiate.
-         This name must be the name that was used when registering the native user-function 
+        op_id (str): Id of the native user-defined Function to instantiate.
+         This must be the id that was used when registering the native user-function 
          with the 'register_native_user_function' method.
         operands (list): input operands of the new instance of the native user-defined Function.
         user_function_instance_name (str): Name of the instance of the created native 
@@ -1222,7 +1222,7 @@ def native_user_function(op_name, operands, attributes=None, user_function_insta
         attributes = {}
 
     attributes = _py_dict_to_cntk_dict(attributes)
-    return cntk_py.Function_native_user_function(op_name, operands, attributes, user_function_instance_name)
+    return cntk_py.Function_native_user_function(op_id, operands, attributes, user_function_instance_name)
 
 @typemap
 def load_model(model, device=None, udf_factory_callback_map=None):
